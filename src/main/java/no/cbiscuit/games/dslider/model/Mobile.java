d01bab1b1c52bdf1307575af11fe9f0f250cdcc7
JRich <jan_hovik@yahoo.com>
2011-10-13 18:38:27 +0200

    Added background and mobiles

--- src/main/java/no/cbiscuit/games/dslider/model/Mobile.java
+++ src/main/java/no/cbiscuit/games/dslider/model/Mobile.java
@@ -167,6 +167,45 @@ public class Mobile {
 		this.speedY = mSpeedY;
 	}
 
-	
-
+	public static float[] crash(float xCo1,float yCo1, float xSpd1, float ySpd1,float xCo2,float yCo2,float xSpd2, float ySpd2 ){
+		float[] crash=new float[4];
+		float dx;
+		float dy;
+		float mass1=1;
+		float mass2=1;
+		float collisionA;
+		float magnitude1;
+		float magnitude2;
+		float direction1;
+		float direction2;
+		float XnewSpeed1;
+		float YnewSpeed1;
+		float XnewSpeed2;
+		float YnewSpeed2;
+		float finalXspeed1;
+		float finalYspeed1;
+		float finalXspeed2;
+		float finalYspeed2;
+		dx = xCo1-xCo2;
+		dy = yCo1-yCo2;
+		collisionA= (float) Math.atan2(dy, dx);
+		magnitude1 = (float) Math.sqrt(xSpd1*xSpd1+ySpd1*ySpd1);
+		magnitude2 = (float) Math.sqrt(xSpd2*xSpd2+ySpd2*ySpd2);
+		direction1 = (float) Math.atan2(ySpd1, xSpd1);
+		direction2 = (float) Math.atan2(ySpd2, xSpd2);
+		XnewSpeed1 = (float) (magnitude1*Math.cos(direction1-collisionA));
+		YnewSpeed1 = (float) (magnitude1*Math.sin(direction1-collisionA));
+		XnewSpeed2 = (float) (magnitude2*Math.cos(direction2-collisionA));
+		YnewSpeed2 = (float) (magnitude2*Math.sin(direction2-collisionA));
+		finalXspeed1 =((mass1-mass2)*XnewSpeed1+(mass1+mass2) *XnewSpeed2)/(mass1+mass2);//the balls have a mass of one
+		finalXspeed2 = ((mass1+mass2)*XnewSpeed1+(mass2-mass1)*XnewSpeed2)/(mass1+mass2);//the balls have a mass of one
+		finalYspeed1 = YnewSpeed1;
+		finalYspeed2 = YnewSpeed2;
+		crash[0]= (float) (Math.cos(collisionA)*finalXspeed1+Math.cos(collisionA*Math.PI/2)*finalYspeed1);
+		crash[1] = (float) (Math.sin(collisionA)*finalXspeed1+Math.sin(collisionA*Math.PI/2)*finalYspeed1);
+		crash[2]= (float) (Math.cos(collisionA)*finalXspeed2+Math.cos(collisionA*Math.PI/2)*finalYspeed2);
+		crash[3]= (float) (Math.sin(collisionA)*finalXspeed2+Math.sin(collisionA*Math.PI/2)*finalYspeed2);
+        
+		return crash;
+		}
 }
